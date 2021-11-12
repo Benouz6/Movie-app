5b96884ee3a146e86a4513c65b6900337226efdb
benoit leboucher <ben.lebou@gmail.com>
2021-11-12 11:56:03 -0500

    Root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
   resources :lists, only: [:index, :new, :show, :create, :edit] do
     resources :bookmarks, only: [:create, :new, :destroy]
   end
