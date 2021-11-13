5f344e377f0560164ee5cb5a192f4a98bca0606b
benoit leboucher <ben.lebou@gmail.com>
2021-11-12 22:47:32 -0500

    redirect path done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,9 +17,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(movie_params)
-    @list.save
-    redirect_to lists_path
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path(@list)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -33,7 +36,7 @@ class ListsController < ApplicationController
 
   private
 
-  def movie_params
+  def list_params
     params.require(:list).permit(:name)
   end
 end
