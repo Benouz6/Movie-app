5f9d4888468c1057db21c0d5b1d09d6312d61ed4
benoit leboucher <ben.lebou@gmail.com>
2021-11-20 14:08:34 -0500

    Some cleaning in the code

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,13 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
   def new
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -25,5 +25,9 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
